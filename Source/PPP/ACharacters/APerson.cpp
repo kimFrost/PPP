c22684e1a95e440332a8e74ca68ee4013ce388e9
kimFrost <kim@thatworks.dk>
2018-01-25 23:03:42 +0100

    ()

--- Source/PPP/ACharacters/APerson.cpp
+++ Source/PPP/ACharacters/APerson.cpp
@@ -7,7 +7,7 @@
 APerson::APerson()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
