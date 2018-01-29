432d30e16d9e6a419e6ce9aff6c89948902ad389
kimFrost <kim@thatworks.dk>
2018-01-29 21:01:46 +0100

    ()

---
+++ Source/PPP/AActors/AStructure.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AStructure.h"
+
+
+// Sets default values
+AStructure::AStructure()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AStructure::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AStructure::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
