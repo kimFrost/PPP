18c9b25a071b0dcf3514634182f704919ed507e3
kim <kim@impact.dk>
2018-02-02 14:23:37 +0100

    ()

---
+++ Source/PPP/UComponents/URoadMovementComponent.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "URoadMovementComponent.h"
+#include "AActors/ARoad.h"
+#include "UObjects/UTile.h"
+#include "UObjects/UTileManager.h"
+#include "Libraries/UTilesLibrary.h"
+
+
+URoadMovementComponent::URoadMovementComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+
+bool URoadMovementComponent::MoveUpdatedComponentImpl(const FVector& Delta, const FQuat& NewRotation, bool bSweep, FHitResult* OutHit, ETeleportType Teleport)
+{
+	if (UpdatedComponent)
+	{
+		//const FVector NewDelta = ConstrainDirectionToPlane(Delta);
+		//return UpdatedComponent->MoveComponent(NewDelta, NewRotation, bSweep, OutHit, MoveComponentFlags, Teleport);
+	}
+
+	return false;
+}
+
+
+void URoadMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+void URoadMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (UpdatedComponent)
+	{
+		const FQuat NewRotationQuat = UpdatedComponent->GetComponentQuat();
+		const FVector Adjustment = FVector::VectorPlaneProject(DeltaTime, Normal) * Time;
+
+		// Move without sweeping.
+		MoveUpdatedComponent(Adjustment, NewRotationQuat, false, nullptr, ETeleportType::TeleportPhysics);
+
+		//bool bMoved = MoveUpdatedComponent(Adjustment, NewRotationQuat, true, &SweepOutHit, ETeleportType::TeleportPhysics);
+	}
+	
+
+
+}
---
+++ Source/PPP/UComponents/URoadMovementComponent.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "URoadMovementComponent.generated.h"
+
+
+//~~~~~ Forward Declarations ~~~~~//
+class UTileManager;
+class ARoad;
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PPP_API URoadMovementComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	URoadMovementComponent();
+
+	UTileManager* TileManager;
+
+	ARoad* RoadOn;
+
+	ARoad* NextTargetRoad;
+
+	USceneComponent* UpdatedComponent;
+
+	TArray<ARoad*> CurrentRoute;
+
+	//GetRoute
+
+
+protected:
+
+	virtual bool MoveUpdatedComponentImpl(const FVector& Delta, const FQuat& NewRotation, bool bSweep, FHitResult* OutHit = NULL, ETeleportType Teleport = ETeleportType::None);
+
+	virtual void BeginPlay() override;
+
+public:	
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+};
