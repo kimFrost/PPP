3b7a36b84807deb4e60d17f3ace71ed24e823687
kimFrost <kim@thatworks.dk>
2018-03-14 20:52:21 +0100

    ()

---
+++ Source/PPP/AActors/ABuilder.cpp
@@ -0,0 +1,139 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Engine/StreamableManager.h"
+#include "Components/ArrowComponent.h"
+#include "Libraries/UGridLibrary.h"
+#include "UObjects/UTile.h"
+#include "ABuilder.h"
+
+
+// Sets default values
+ABuilder::ABuilder()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Rotation = 0;
+	bIsBuildValid = false;
+}
+/*
+void ABuilder::SetData(FST_Structure& Data)
+{
+	StructureBaseData = Data;
+
+	//https://docs.unrealengine.com/latest/INT/Programming/Assets/ReferencingAssets/
+
+	UStaticMesh* PreviewMesh = LoadMesh(StructureBaseData.PreviewMesh);
+	if (PreviewMesh)
+	{
+		Mesh->SetStaticMesh(PreviewMesh);
+	}
+	else if (DefaultMesh)
+	{
+		Mesh->SetStaticMesh(DefaultMesh);
+	}
+}
+*/
+
+UStaticMesh* ABuilder::LoadMesh(TAssetPtr<UStaticMesh> MeshAssetID)
+{
+	if (MeshAssetID.IsPending())
+	{
+		const FStringAssetReference& AssetRef = MeshAssetID.ToStringReference();
+		MeshAssetID = Cast<UStaticMesh>(AssetLoader.LoadSynchronous(AssetRef, true));
+	}
+	return MeshAssetID.Get();
+}
+
+int32 ABuilder::Rotate(int32 Direction, UTile * Tile)
+{
+	Rotation = (Rotation + 1) % 6;
+	SetActorRotation(FRotator(0, Rotation * (360 / 6), 0));
+	if (Tile)
+	{
+		SetRootTile(Tile);
+	}
+	UpdateTiles();
+	return Rotation;
+}
+
+void ABuilder::SetRootTile(UTile* Tile)
+{
+	if (Tile)
+	{
+		FHitResult HitResult(ForceInit);
+		SetActorLocation(Tile->WorldLocation, false, &HitResult, ETeleportType::TeleportPhysics);
+		RootTile = Tile;
+		UpdateTiles();
+	}
+}
+
+
+void ABuilder::UpdateTiles()
+{
+	// If RootHex is occupied
+	/*
+	if (RootHex && RootHex->AttachedBuilding)
+	{
+		if (StructureBaseData.AttachTo.Contains(RootHex->AttachedBuilding->StructureBaseData.Id))
+		{
+			UHexTile* NewAttachPlacement = RootHex->GetNeighbourByOffset(UPOTLUtilFunctionLibrary::DirectionToCube(Rotation + 3)); // +3 to flip rotation
+			if (NewAttachPlacement)
+			{
+				SetRootHex(NewAttachPlacement);
+				return;
+			}
+		}
+	}
+	*/
+
+	// Get all tiles on
+	/*
+	TilesOn.Empty();
+	for (int32 i = 0; i < StructureBaseData.CubeSizes.Num(); i++)
+	{
+		FVector CubeCoord = StructureBaseData.CubeSizes[i];
+		FVector RotatedCubeCoord = UPOTLUtilFunctionLibrary::RotateCube(CubeCoord, Rotation, FVector(0, 0, 0));
+		UHexTile* OffsetHex = RootHex->GetNeighbourByOffset(RotatedCubeCoord);
+		TilesOn.Add(OffsetHex);
+		if (OffsetHex)
+		{
+			DrawDebugString(GetWorld(), OffsetHex->Location, "Tile", nullptr, FColor::Blue, 0.2, true);
+		}
+	}
+	*/
+
+	bIsBuildValid = ValidatePlacement();
+}
+bool ABuilder::ValidatePlacement()
+{
+	if (RootTile)
+	{
+
+	}
+	return false;
+}
+void ABuilder::Hide()
+{
+	SetActorHiddenInGame(true);
+}
+void ABuilder::Show()
+{
+	SetActorHiddenInGame(false);
+}
+
+// Called when the game starts or when spawned
+void ABuilder::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABuilder::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+//https://www.youtube.com/watch?v=aFmLu4LSm8s
---
+++ Source/PPP/AActors/ABuilder.h
@@ -0,0 +1,89 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Engine/StreamableManager.h"
+#include "ABuilder.generated.h"
+
+
+//~~~~~ Forward Declarations ~~~~~//
+class UArrowComponent;
+class UTile;
+
+
+UCLASS()
+class PPP_API ABuilder : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABuilder();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Builder") //EditDefaultsOnly 
+	UStaticMesh* DefaultMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Builder")
+	UArrowComponent* Arrow;
+
+private:
+
+	UStaticMeshComponent* Mesh;
+	FStreamableManager AssetLoader;
+
+	UTile* RootTile;
+	TArray<UTile*> TilesOn;
+	int32 Rotation;
+	bool bIsBuildValid;
+	//FST_Structure Data;
+
+
+private:
+
+	UStaticMesh* LoadMesh(TAssetPtr<UStaticMesh> MeshAssetID);
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Builder")
+	int32 Rotate(int32 Direction, UTile* Tile);
+	 
+	//UFUNCTION(BlueprintCallable, Category = "Builder")
+	//void SetData(FST_Structure& Data);
+
+	void SetRootTile(UTile* Tile);
+
+
+	// Sizes as a array of coords 
+
+	void UpdateTiles();
+	bool ValidatePlacement();
+
+	void Hide();
+	void Show();
+
+
+
+	//bool Stamp(); // Stamp a copy of the builder
+
+	//Set builder class
+
+	// Set location on grid
+
+	// Validate placement
+
+
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
